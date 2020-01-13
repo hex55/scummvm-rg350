673c0a61c0a764c8ab605803fc393f8e8c565c65
craigsc <craigsc333@gmail.com>
2020-01-12 22:33:00 -0800

    Add the top-level dir of the packaged OPK into the LD_LIBRARY_PATH to use the libmad lib dynamically

--- dists/gcw0/scummvm.sh
+++ dists/gcw0/scummvm.sh
@@ -6,6 +6,8 @@ if [ ! -f $HOME/.scummvmrc ] ; then
 	cp ./scummvmrc $HOME/.scummvmrc
 fi
 
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+
 if [ ! -f $HOME/.debug_scummvmrc ] ; then
 	exec ./scummvm 2>&1 >/var/tmp/scummvm.log
 else
