9d99cfe35e7d90ea572b989167f7711cb1e9316c
goldmojo <goldmojo@free.fr>
2020-04-23 20:08:58 +0200

    Starts ScummVM with gameid.svm as first arg

--- dists/gcw0/scummvm.sh
+++ dists/gcw0/scummvm.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+romPath=`dirname "$1"`
+romName=`basename "$1"`
+
 cd `dirname $0`
 
 if [ ! -f $HOME/.scummvmrc ] ; then
@@ -9,7 +12,8 @@ fi
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 
 if [ ! -f $HOME/.debug_scummvmrc ] ; then
-	exec ./scummvm 2>&1 >/var/tmp/scummvm.log
+	exec ./scummvm -f -p"$romPath" ${romName%%.*} 2>&1 >/var/tmp/scummvm.log
 else
-	exec ./scummvm -d 11 2>&1 >/var/tmp/scummvm.log
+	exec ./scummvm -d 11 -f -p"$romPath" ${romName%%.*} 2>&1 >/var/tmp/scummvm.log
 fi
+
