c8508b0b44817d7365cc57fc21f20761be49feb7
salvacam <salvacams@gmail.com>
2021-01-31 02:48:18 +0100

    fix varible compile

--- backends/platform/dingux/build.miyoo.sh
+++ backends/platform/dingux/build.miyoo.sh
@@ -4,7 +4,7 @@ export PATH=/opt/miyoo/usr/bin/:$PATH
 
 # ../../../configure --host=miyoo --with-mad-prefix=/opt/miyoo/usr/bin --enable-plugins --default-dynamic --disable-all-unstable-engines --disable-flac --disable-debug --disable-highres --enable-release-mode --disable-hq-scalers && make && make miyoo-dist  
 
- ../../../configure --host=miyoo --with-mad-prefix=/opt/miyoo/usr/bin --enable-plugins --default-dynamic --disable-all-unstable-engines --disable-flac --disable-debug --disable-highres --enable-release-mode --disable-hq-scalers --disable-mt32emu --disable-lua --disable-nuked-opl && make && make miyoo-dist  
+ ../../../configure --host=miyoo --with-mad-prefix=/opt/miyoo/usr/bin --enable-plugins --default-dynamic --disable-all-unstable-engines --disable-flac --disable-debug --disable-highres --enable-release-mode --disable-hq-scalers --disable-mt32emu --disable-lua --disable-nuked-opl && make && make miyoo-dist
 
 #este es el bueno
 #../../../configure --host=miyoo --with-mad-prefix=/opt/miyoo/usr/bin --enable-plugins --disable-all-unstable-engines --default-dynamic --disable-flac --disable-highres --disable-debug --disable-hq-scalers --enable-release && make && make miyoo-dist
